9411bbd85aa832396bd7c127daf2b85ee30c1ca3
Jon Sousa <jonathonsousa@gmail.com>
2022-12-11 14:19:41 -0600

    Initial commit with some basic player moves

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        float speed = 3.5f;
+
+        Rigidbody2D rigidbody;
+        BoxCollider2D collider;
+
+        private void Awake()
+        {
+            rigidbody = gameObject.AddComponent<Rigidbody2D>();
+            collider = gameObject.AddComponent<BoxCollider2D>();
+        }
+
+        private void Start()
+        {
+            rigidbody.isKinematic = true;
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            var horizontalMovement = Input.GetAxisRaw("Horizontal");
+            var verticallMovement = Input.GetAxisRaw("Vertical");
+
+            Vector2 movement = new Vector2(horizontalMovement * speed, verticallMovement * speed);
+
+            movement *= Time.deltaTime * 100; //This makes movement smoother
+            rigidbody.velocity = movement;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/ScreenBoundaries.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class ScreenBoundaries : MonoBehaviour
+    {
+        [Tooltip("How far from the side edges of the screen you want the player to be.")]
+        [SerializeField, Range(-5f, 5f)]
+        float horizontalBoundary = 1;
+
+        [Tooltip("How far from the top or bottom edge of the screen you want the player to be.")]
+        [SerializeField, Range(-10f, 10f)]
+        float verticalBoundary = 0.65f;
+
+        float objectWidth;
+        float objectHeight;
+        Vector2 screenBounds;
+
+        private void Start()
+        {
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+            objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+            objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 viewPosition = transform.position;
+
+            viewPosition.x = Mathf.Clamp(viewPosition.x, screenBounds.x * -1 - objectWidth + horizontalBoundary, screenBounds.x + objectWidth - horizontalBoundary);
+            viewPosition.y = Mathf.Clamp(viewPosition.y, screenBounds.y * -1 - objectHeight + verticalBoundary, screenBounds.y + objectHeight - verticalBoundary);
+
+            transform.position = viewPosition;
+        }
+    }
+}
