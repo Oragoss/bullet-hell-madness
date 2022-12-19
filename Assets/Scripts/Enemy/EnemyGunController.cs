c80d49b4d67fee7508cdee29c68cece497184789
Jon Sousa <jonathonsousa@gmail.com>
2022-12-19 14:50:17 -0600

    Added new lasers and new enemies. Enemies don't fire until on screen

--- Assets/Scripts/Enemy/EnemyGunController.cs
+++ Assets/Scripts/Enemy/EnemyGunController.cs
@@ -8,7 +8,7 @@ namespace Assets.Scripts.Enemy
 
         [SerializeField] int shots = 1;
         [SerializeField] float fireRate = 1;
-        [SerializeField] float delayBeforeFiring = 0.5f;
+        [SerializeField] float delayBeforeFiring = 2.5f;
         [SerializeField] EnemyBulletController enemyBulletController;
 
         public int accuracy = 100;
@@ -29,18 +29,6 @@ namespace Assets.Scripts.Enemy
         private void FireSequence()
         {
             enemyBulletController.Fire(shots);
-        }
-
-        private void Update()
-        {
-            //if (Input.GetButton("Fire1"))
-            //{
-            //    if (Time.time >= fireRateCounter)
-            //    {
-            //        bulletController.Fire(shots);
-            //        fireRateCounter = Time.time + 1 / fireRate; //fireRate is how many bullets can be shot for every 1 second. Change the 1 to a 2 and it's the same number of shots in 2 seconds.
-            //    }
-            //}
         }
     }
 }
