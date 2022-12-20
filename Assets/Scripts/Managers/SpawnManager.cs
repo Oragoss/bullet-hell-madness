57080cec0faba457cad1abd45afbde8c311ccb18
Jon Sousa <jonathonsousa@gmail.com>
2022-12-20 16:17:47 -0600

    Can now spawn in objects that will float by

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -9,26 +9,43 @@ namespace Assets.Scripts.Managers
     {
         public static SpawnManager spawnManager;
 
+        [Header("Enemy Variables")]
         public float spawnWaveDelay = 0;
         public float waveRepeatRate = 2.5f;
 
         [Tooltip("Regular enemies to spawn."), SerializeField]
         private List<EnemyWave> wave = new List<EnemyWave>();
 
+        [Header("Obstacle Variables")]
+        [Tooltip("Waves that will spawn in player obstacles."), SerializeField]
+        private List<EnemyWave> obstacleWave = new List<EnemyWave>();
+        
+        [SerializeField]
+        private int nextObjectSpawnTime;
+        
+        [SerializeField]
+        private bool spawnObstacleWaveInsteadOfEnemyWave;
+
+        [Tooltip("The minimum number of seconds before an obstacle is spawned."), SerializeField]
+        private int objectTimerMinRange = 5;
+        [Tooltip("The maximum number of seconds before an obstacle is spawned."), SerializeField]
+        private int objectTimerMaxRange = 300;
+
+        private float obstacleTimer;
+
+        [Header("Special Enemy Variables")]
         [Tooltip("Special enemies that have more powerful attacks and stats."), SerializeField]
         private List<EnemyWave> specialWave = new List<EnemyWave>();
 
-        [Tooltip("Waves that will spawn in player obstacles."), SerializeField]
-        private List<EnemyWave> obstacleWave = new List<EnemyWave>();
-
-        [SerializeField] private int nextSpecialSpawnTime;
+        [SerializeField] 
+        private int nextSpecialSpawnTime;
 
         [Tooltip("The minimum number of seconds before a special is spawned."), SerializeField]
-        private int timerMinRange = 25;
+        private int specialEnemyTimerMinRange = 25;
         [Tooltip("The maximum number of seconds before a special is spawned."), SerializeField]
-        private int timerMaxRange = 120;
+        private int specialEnemyTimerMaxRange = 120;
 
-        private float timer;
+        private float specialEnemyTimer;
 
         [Header("Testing Variables")]
         [SerializeField, Tooltip("Turns on the Spawn Manager's test mode which will allow you to test specific features. Make sure this is turned off for production.")]
@@ -64,29 +81,48 @@ namespace Assets.Scripts.Managers
         {
             StartSpawnWaves();
             var random = new System.Random(Mathf.Abs(Guid.NewGuid().GetHashCode()));
-            nextSpecialSpawnTime = random.Next(timerMinRange, timerMaxRange);
+            nextSpecialSpawnTime = random.Next(specialEnemyTimerMinRange, specialEnemyTimerMaxRange);
+            nextObjectSpawnTime = random.Next(objectTimerMinRange, objectTimerMaxRange);
         }
 
         private void FixedUpdate()
         {
-            timer += Time.deltaTime;
-            if (timer >= nextSpecialSpawnTime)
+            specialEnemyTimer += Time.deltaTime;
+            if (specialEnemyTimer >= nextSpecialSpawnTime)
                 SpawnSpecial();
+
+            obstacleTimer += Time.deltaTime;
+            if (obstacleTimer >= nextObjectSpawnTime)
+            {
+                var random = new System.Random(Mathf.Abs(Guid.NewGuid().GetHashCode()));
+                nextObjectSpawnTime = random.Next(objectTimerMinRange, objectTimerMaxRange);
+                obstacleTimer = 0;
+                spawnObstacleWaveInsteadOfEnemyWave = true;
+            }
         }
 
         private void SpawnWaves()
         {
             var random = new System.Random(Mathf.Abs(Guid.NewGuid().GetHashCode()));
             int next;
+            EnemyWave currentWave;
 
             if (testMode)
             {
                 next = waveToTest;
+                currentWave = wave[next];
             }
+            else if (spawnObstacleWaveInsteadOfEnemyWave)
+            {
+                next = random.Next(0, obstacleWave.Count);
+                currentWave = obstacleWave[next];
+                spawnObstacleWaveInsteadOfEnemyWave = false;
+            } 
             else
+            {
                 next = random.Next(0, wave.Count);
-
-            var currentWave = wave[next];
+                currentWave = wave[next];
+            }
 
             for (var y = 0; y < currentWave.enemyBehavior.Count; y++)
             {
@@ -100,7 +136,7 @@ namespace Assets.Scripts.Managers
         private void SpawnSpecial()
         {
             var random = new System.Random(Mathf.Abs(Guid.NewGuid().GetHashCode()));
-            nextSpecialSpawnTime = random.Next(timerMinRange, timerMaxRange);
+            nextSpecialSpawnTime = random.Next(specialEnemyTimerMinRange, specialEnemyTimerMaxRange);
             int next = random.Next(0, specialWave.Count);
 
             StopSpawnWaves();
@@ -117,7 +153,7 @@ namespace Assets.Scripts.Managers
             }
 
             StartSpawnWaves();
-            timer = 0;
+            specialEnemyTimer = 0;
         }
     }
 }
