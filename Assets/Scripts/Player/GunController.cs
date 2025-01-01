b3daf6c8b821f1b3cb4629e344afaf55beb9da79
Jon Sousa <jonathonsousa@gmail.com>
2025-01-01 13:57:57 -0600

    fixed some bugs with the build. The game is a bit janky now unfortunately. Unity needs to get it's shit together

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -28,14 +28,11 @@ namespace Assets.Scripts.Player
         {
             if (Input.GetButton("Fire1"))
             {
-                if (playerControl.health > 0)
+                if (Time.time >= fireRateCounter)
                 {
-                    if (Time.time >= fireRateCounter)
-                    {
-                        bulletController.Fire(shots);
-                        fireRateCounter = Time.time + 1 / fireRate; //fireRate is how many bullets can be shot for every 1 second. Change the 1 to a 2 and it's the same number of shots in 2 seconds.
-                        gunSound.Play();
-                    }
+                    bulletController.Fire(shots);
+                    fireRateCounter = Time.time + 1 / fireRate; //fireRate is how many bullets can be shot for every 1 second. Change the 1 to a 2 and it's the same number of shots in 2 seconds.
+                    gunSound.Play();
                 }
             }
         }
